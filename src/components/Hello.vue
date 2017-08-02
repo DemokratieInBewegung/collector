2b60c56cc4ba7828f6f3eb4698c3e084edb107a8
Benjamin Kampmann <ben@gnunicorn.org>
2017-08-02 02:28:41 +0200

    wizard puts to pouch

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <form-wizard @on-complete="onComplete"
+    <form-wizard @on-complete="submitModel"
+                 ref="wizard"
                  color="gray"
                  title="Kontakt Informationen eintragen"
                  subtitle="für DEMOKRATIE IN BEWEGUNG"
@@ -19,8 +20,8 @@
                                  
            </vue-form-generator>
         </tab-content>
-        <tab-content title="Kontakt Info"
-                     icon="ti-id-badge" :before-change="submitModel">
+        <tab-content title="Kontakt"
+                     icon="ti-id-badge" :before-change="validateSecondTab">
          <vue-form-generator :model="model" 
                                :schema="secondTabSchema"
                                :options="formOptions"
@@ -29,35 +30,59 @@
            </vue-form-generator>
            
         </tab-content>
-        <tab-content title="Danke"
-                     icon="ti-heart">
-          <h4>Your json is ready!</h4>
-          <div class="panel-body">
-            <pre v-if="model" v-html="prettyJSON(model)"></pre>
-          </div>
+        <tab-content title="Extras"
+                     icon="ti-heart" :before-change="validateThirdTab">
+          <h2>Deine Einschätzung zu der Person</h2>
+            
+           <vue-form-generator :model="model" 
+                                 :schema="thirdTabSchema"
+                                 :options="formOptions"
+                                 ref="thirdTabForm"
+                                 >                                
+             </vue-form-generator>
         </tab-content>
-    </form-wizard>
+    </form-wizard> 
   </div>
 </template>
 
 <script>
 import VueFormGenerator from 'vue-form-generator'
 // import VueFormWizard from 'vue-form-wizard'
+import PouchDB from 'pouchdb'
+import uuid from 'uuid'
 
 import 'vue-form-wizard/dist/vue-form-wizard.min.css'
+
+const db = new PouchDB('collector')
+
+window.db = db
+window.uuid = uuid
+
+db.replicate.to('http://example.com/mydb', {
+  live: true,
+  retry: true
+})
+
+function makeModel () {
+  return {
+    firstName: '',
+    lastName: '',
+    email: '',
+    newsletter: true,
+    postal_code: '',
+    phone_number: '',
+    whatsapp: false,
+    gender: '',
+    age: '< 25',
+    confidence: 0
+  }
+}
 
 export default {
   name: 'wizard',
   data () {
     return {
-      model: {
-        firstName: '',
-        lastName: '',
-        email: '',
-        newsletter: true,
-        postal_code: '',
-        phone_number: ''
-      },
+      model: makeModel(),
       formOptions: {
         validationErrorClass: 'has-error',
         validationSuccessClass: 'has-success',
@@ -124,47 +149,81 @@ export default {
             required: false,
             validator: VueFormGenerator.validators.string,
             styleClasses: 'col-md-6 offset-md-3'
+          },
+          {
+            type: 'checkbox',
+            label: 'Ist eine Whatsapp Nummer',
+            model: 'whatsapp',
+            default: true,
+            styleClasses: 'col-md-6 offset-md-3 form-inline'
+          }
+        ]
+      },
+      thirdTabSchema: {
+        fields: [
+          {
+            type: 'radios',
+            autocomplete: false,
+            label: 'Geschlecht',
+            model: 'gender',
+            required: false,
+            styleClasses: 'col-md-6 offset-md-3 h4',
+            values: [
+               { name: 'Mann', value: 'm' },
+               { name: 'Frau', value: 'f' },
+               { name: 'Weder-noch', value: 'o' }]
+          },
+          {
+            type: 'select',
+            autocomplete: false,
+            label: 'Alter (circa)',
+            model: 'age',
+            required: false,
+            styleClasses: 'col-md-6 offset-md-3 h4',
+            values: [
+               { name: '< 25', value: '< 25' },
+               { name: '25-35', value: '25-35' },
+               { name: '35-45', value: '35-45' },
+               { name: '45-55', value: '45-55' },
+               { name: '55-65', value: '55-65' },
+               { name: '65-75', value: '65-75' },
+               { name: '> 75', value: '> 75' }]
+          },
+          {
+            type: 'radios',
+            autocomplete: false,
+            label: 'Wird uns wählen Sicherheit:',
+            model: 'confidence',
+            required: false,
+            styleClasses: 'col-md-6 offset-md-3 h3',
+            values: [
+               { name: ' ☹ ', value: 1 },
+               { name: ' 😐 ', value: 2 },
+               { name: ' 🤔 ', value: 3 },
+               { name: ' 🙂 ', value: 4 },
+               { name: ' 😍 ', value: 5 }]
           }
         ]
       }
     }
   },
   methods: {
-    onComplete: function () {
-      alert('Yay. Done!')
-    },
     validateFirstTab: function () {
       return this.$refs.firstTabForm.validate()
     },
     validateSecondTab: function () {
       return this.$refs.secondTabForm.validate()
     },
-    submitModel: function () {
-      console.log(this)
-      let validated = this.validateSecondTab()
-      if (!validated) return validated
-      console.log(this.model)
+    validateThirdTab: function () {
+      return this.$refs.thirdTabForm.validate()
     },
-    prettyJSON: function (json) {
-      if (json) {
-        json = JSON.stringify(json, undefined, 4)
-        json = json.replace(/&/g, '&').replace(/</g, '<').replace(/>/g, '>')
-        return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\\-]?\d+)?)/g, function (match) {
-          var cls = 'number'
-          if (/^"/.test(match)) {
-            if (/:$/.test(match)) {
-              cls = 'key'
-            } else {
-              cls = 'string'
-            }
-          } else if (/true|false/.test(match)) {
-            cls = 'boolean'
-          } else if (/null/.test(match)) {
-            cls = 'null'
-          }
-          return '<span class="' + cls + '">' + match + '</span>'
-        })
-      }
+    submitModel: function () {
+      let model = JSON.parse(JSON.stringify(this.model))
+      model._id = uuid()
+      db.put(model).then(() => {
+        this.$data.model = makeModel()
+        this.$refs.wizard.navigateToTab(0)
+      })
     }
   }
 }
